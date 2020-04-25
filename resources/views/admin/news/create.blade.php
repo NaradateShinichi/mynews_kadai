41b0e89ae49dd5ba4eb605d89e2887d65872d0ab
楢館真市 <naraaa.tx512zz@gmail.com>
2020-04-25 08:57:36 +0000

    first commit

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,10 +1,9 @@
-
 {{-- layouts/admin.blade.phpを読み込む --}}
 @extends('layouts.admin')
 
 
 {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title','MyProfile')
+@section('title','ニュースの新規作成')
 
 
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
@@ -12,7 +11,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>Myプロフィール</h2>
+                <h2>ニュース新規作成</h2>
             </div>
         </div>
     </div>
